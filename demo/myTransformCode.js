bcdbd91dfcfd0009e52714a4f0ebef3d258c6f42
liujiang <1462547921@qq.com>
2023-03-06 19:22:43 +0800

    add:封包拆包实现

---
+++ demo/myTransformCode.js
@@ -0,0 +1,62 @@
+class myTransformCode{
+  constructor() {
+    this.packageNameheaderLen = 4
+    this.serialNum=0
+    this.serialLen= 2
+  }
+
+  /**
+   * @description  编码
+   * @author 柳江
+   * @date 06/03/2023
+   * @param {*} data
+   * @param {*} seralNum
+   * @memberof myTransformCode
+   */
+  encode(data,seralNum) {
+    const body = Buffer.from(data )
+    
+    // 开辟空间 作为
+    const headerbuf = Buffer.alloc(this.packageNameheaderLen) 
+    headerbuf.writeInt16BE(seralNum|| this.serialNum)
+    headerbuf.writeInt16BE(body.length,this.serialLen)
+    if (seralNum === undefined) {
+      seralNum++ 
+    }
+     return Buffer.concat([headerbuf,body])
+  }
+
+  /**
+   * @description 解码
+   * @author 柳江
+   * @date 06/03/2023
+   * @param {*} buffer
+   * @returns {*} 
+   * @memberof myTransformCode
+   */
+  decode(buffer){
+    const headerbuf = buffer.slice(0,this.packageNameheaderLen)
+    const bodybuf =buffer.slice(this.packageNameheaderLen)
+    return {
+      seralNum:headerbuf.readInt16BE(),
+      bodyLength:headerbuf.readInt16BE(this.serialLen),
+      body:bodybuf.toString()
+    }
+  } 
+
+  /**
+   * @description 获取包的长度
+   * @author 柳江
+   * @date 06/03/2023
+   * @memberof myTransformCode
+   */
+  getPackageLen(buffer){
+    if (buffer.length < this.packageNameheaderLen) {
+        return 0
+    } else {
+        return this.packageNameheaderLen + buffer.readInt16BE(this.serialLen)
+    }
+  }
+}
+
+module.exports = myTransformCode
---
+++ demo/test.js
@@ -0,0 +1,27 @@
+const myTransformCode = require('./myTransformCode')
+
+let ts = new myTransformCode()
+let str1 = '学前教育'
+console.log(Buffer.from(str1));
+console.log(ts.encode(str1,1));
+const temp = ts.encode(str1,1)
+
+let encodstr = ts.decode(temp)
+console.log(encodstr);
+
+let len = ts.getPackageLen(temp)
+console.log(len );
+// function test(data) {
+//   Number.prototype.add = function (count) { return Number(this + count) }
+//   Number.prototype.subtract = function (count) { return Number(this - count) }
+//   Number.prototype.multiply = function (count) { return Number(this * count) }
+//   Number.prototype.division = function (count) { return Number(this / count) }
+//   return Number(data)
+// }
+
+
+// const aaa = new Array(100)
+
+// console.log(test(121).add(1));
+
+
