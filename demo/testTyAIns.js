a150f81723278883927c5805896783a5cdcd939d
liujiang <1462547921@qq.com>
2023-03-22 20:39:19 +0800

    fix: 调试

--- demo/testTyAIns.js
+++ demo/testTyAIns.js
@@ -9,6 +9,13 @@ console.log(Object.getOwnPropertyNames(as))
 const asdf = [as, a, null, () => {}]
 const aass = ['sdf', 'add']
 console.log(Object.getOwnPropertyNames(asdf))
-
+const obj = new Object()
+// eslint-disable-next-line @typescript-eslint/no-implied-eval
+const fun = new Function()
+const Object1 = Object
+console.log(obj, 'obj')
+console.log(fun, 'fun')
+console.log(Object)
 console.log(Function instanceof Object)
 console.log(Object instanceof Function)
+
