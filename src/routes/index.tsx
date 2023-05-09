31f7c638f029091126e7a3072ed3b2293e6f8f94
liujiang <1462547921@qq.com>
2023-05-09 22:53:45 +0800

    fix: 路由配置

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,8 @@ import Layout from '@/layout/index'
 import { Navigate } from 'react-router'
 import { useRoutes } from 'react-router-dom'
 import Home from '@/pages/Home/index'
+import Login from '@/pages/Login/index'
+// import menu from '@/mock/menu.json'
 // import Dashboard from '@/pages/dashboard';
 // import LoginPage from '@/pages/login';
 // const Home = lazy(() => import(/* webpackChunkName: "404'"*/ '@/pages/Home/index'))
@@ -12,10 +14,10 @@ import Home from '@/pages/Home/index'
 // 导入你的路由组件
 
 const routes: RouteObject[] = [
-  // {
-  //   path: '/login',
-  //   element: <WrapperRouteComponent element={<LoginPage />} titleId="title.login" />,
-  // },
+  {
+    path: '/login',
+    element: <Login />,
+  },
   {
     path: '/',
     element: <Layout />,
